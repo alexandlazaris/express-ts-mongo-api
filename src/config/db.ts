96496bbcbc43c82800bb7b704c76e4af4739bb0f
Alexander Lazaris <alex.lazaris617@gmail.com>
2025-11-26 20:38:39 +1100

    feat: db connection file

---
+++ src/config/db.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+// import { MongoClient, Db } from 'mongodb';
+// let cachedClient: MongoClient | null = null;
+// let cachedDb: Db | null = null;
+
+const MONGO_URI = process.env.MONGO_URI || '';
+
+export const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('Mongodb connected');
+    }
+    catch (error) {
+        console.log(MONGO_URI);
+        console.error('mongodb connection failed:', error);
+        process.exit(1);
+    }
+};
+
+
+// export async function connectDB(): Promise<Db> {
+
+// if (cachedDb) {
+//     console.log('returning to cached db');
+//     return cachedDb;
+// }
+// const MONGO_URI = process.env.MONGO_URI;
+// if (!MONGO_URI) {
+//     throw new Error('mongo db uri not found in .env');
+// }
+
+// cachedClient = new MongoClient(MONGO_URI);
+// console.log('connecting to db');
+// await cachedClient.connect();
+// console.log('connected to db');
+// const dbName = process.env.DB_NAME || 'test';
+// cachedDb = cachedClient.db(dbName);
+// return cachedDb;
+// }
